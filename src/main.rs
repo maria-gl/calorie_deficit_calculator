2a0764778542693b9359050a3e2652134d426ac4
maria-gl <marigladvova@gmail.com>
2026-04-28 23:20:42 +0300

    implemented enum for gender variable

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,28 @@
 use std::io;
 use std::process::exit;
 
+enum Sex {
+    M,
+    F,
+}
+
+
 fn main() {
     println!("          CALORIE CALCULATOR");
 
     println!("Please, enter the following data to calculate your BMR");
 
     println!("Sex (m/f): ");
-    let mut sex = String::new();
-    io::stdin().read_line(&mut sex).expect("failed to read line");
-    let sex: char = sex.trim().parse().expect("please enter a valid value. Valid values: m, f");
-    if sex == 'm' || sex == 'M'{
-        println!("M!");
+    let mut sex_input = String::new();
+    io::stdin().read_line(&mut sex_input).expect("failed to read line");
+    let sex_input: char = sex_input.trim().parse().expect("Invalid character");
+
+    let sex : Sex;
+    if sex_input == 'm' || sex_input == 'M'{
+        sex = Sex::M;
     }
-    if sex == 'f' || sex == 'F'{
-        println!("F!");
+    else if sex_input == 'f' || sex_input == 'F'{
+        sex = Sex::F;
     }
     else {
         println!("Invalid value. Valid values: m, f");
@@ -36,11 +44,12 @@ fn main() {
     io::stdin().read_line(&mut height).expect("failed to read line");
     let height: f32 = height.trim().parse().expect("please type a number");
 
-    if sex == 'm' || sex == 'M'{
-        let bmr: f32 = 9.99 * weight + 6.25 * height - 4.92 * age + 5.0;
+    let bmr: f32;
+    if let Sex::M = sex {
+        bmr = 9.99 * weight + 6.25 * height - 4.92 * age + 5.0;
     }
     else {
-        let bmr: f32 = 9.99 * weight + 6.25 * height - 4.92 * age + 161.0;
+        bmr = 9.99 * weight + 6.25 * height - 4.92 * age - 161.0;
     }
 
     println!("Your BMR is: {bmr}")
