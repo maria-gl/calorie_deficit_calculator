62beb18320a17938be1f12aecc9583467c386229
maria-gl <marigladvova@gmail.com>
2026-05-04 06:17:02 +0300

    organized code using functions

--- src/main.rs
+++ src/main.rs
@@ -8,48 +8,21 @@ enum Sex {
 
 
 fn main() {
-    println!("          CALORIE CALCULATOR");
+   println!("          CALORIE CALCULATOR");
 
-   let bmr: f32 = calculate_bmr();
+    let sex: Sex = get_sex();
+    let age: f32 = get_age();
+    let height: f32 = get_heigt();
+    let weight: f32 = get_weight();
+
+   let bmr: f32 = calculate_bmr(sex, weight, height, age);
    calculate_clories(bmr);
-    
 }
 
-fn calculate_bmr() -> f32
-{
+
+fn calculate_bmr(sex: Sex, weight: f32, height: f32, age: f32) -> f32 {
+
     println!("Please, enter the following data to calculate your BMR");
-
-    println!("Sex (m/f): ");
-    let mut sex_input = String::new();
-    io::stdin().read_line(&mut sex_input).expect("failed to read line");
-    let sex_input: char = sex_input.trim().parse().expect("Invalid character");
-
-    let sex : Sex;
-    if sex_input == 'm' || sex_input == 'M'{
-        sex = Sex::M;
-    }
-    else if sex_input == 'f' || sex_input == 'F'{
-        sex = Sex::F;
-    }
-    else {
-        println!("Invalid value. Valid values: m, f");
-        exit(1);
-    }
-
-    println!("Age: ");
-    let mut age = String::new();
-    io::stdin().read_line(&mut age).expect("failed to read line");
-    let age: f32 = age.trim().parse().expect("please type a number");
-
-    println!("Weight (kg): ");
-    let mut weight = String::new();
-    io::stdin().read_line(&mut weight).expect("failed to read line");
-    let weight: f32 = weight.trim().parse().expect("please type a number");
-
-    println!("Height (cm): ");
-    let mut height = String::new();
-    io::stdin().read_line(&mut height).expect("failed to read line");
-    let height: f32 = height.trim().parse().expect("please type a number");
 
     let bmr: f32;
     if let Sex::M = sex {
@@ -87,4 +60,57 @@ fn calculate_clories(bmr: f32)
 
     let calories : f32 = bmr * activity_multiplier;
     println!("Your calorie deficit is {} calories", calories);
+}
+
+
+fn get_sex() -> Sex  {
+
+    println!("Sex (m/f): ");
+    let mut sex_input = String::new();
+    io::stdin().read_line(&mut sex_input).expect("failed to read line");
+    let sex_input: char = sex_input.trim().parse().expect("Invalid character");
+
+    let sex : Sex;
+    if sex_input == 'm' || sex_input == 'M'{
+        sex = Sex::M;
+    }
+    else if sex_input == 'f' || sex_input == 'F'{
+        sex = Sex::F;
+    }
+    else {
+        println!("Invalid value. Valid values: m, f");
+        exit(1);
+    }
+
+    return sex;
+}
+
+
+fn get_age() -> f32 {
+    println!("Age: ");
+    let mut age = String::new();
+    io::stdin().read_line(&mut age).expect("failed to read line");
+    let age: f32 = age.trim().parse().expect("please type a number");
+
+    return age;
+}
+
+
+fn get_weight() -> f32 {
+    println!("Weight (kg): ");
+    let mut weight = String::new();
+    io::stdin().read_line(&mut weight).expect("failed to read line");
+    let weight: f32 = weight.trim().parse().expect("please type a number");
+
+    return weight;
+}
+
+
+fn get_heigt() -> f32 {
+    println!("Height (cm): ");
+    let mut height = String::new();
+    io::stdin().read_line(&mut height).expect("failed to read line");
+    let height: f32 = height.trim().parse().expect("please type a number");
+
+    return height;
 }
